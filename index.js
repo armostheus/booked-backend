3486b8ab53802c7caf62c18c3ad9d71caa4f7fd9
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-26 18:38:50 +0530

    added sqlite module and DB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./booked.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+  });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
