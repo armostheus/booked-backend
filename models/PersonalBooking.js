170a1ed88ca1894c2b40e505daec8f4881aba058
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-28 19:14:13 +0530

    finally connected and transacted to mongoAtlas

--- models/PersonalBooking.js
+++ models/PersonalBooking.js
@@ -4,6 +4,39 @@ const PersonalBookingSchema = mongoose.Schema({
     user: {
         type: String,
         required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: false
+    },
+    endTime: {
+        type: Date,
+        required: false
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    private: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    reminder: {
+        type: Boolean,
+        required: false
+    },
+    reminderTime: {
+        type: Date,
+        required: false
     }
 });
 
--- routes/createRoutes.js
+++ routes/createRoutes.js
@@ -5,19 +5,24 @@ const PersonalBooking = require('../models/PersonalBooking')
 //Routes for create APIs
 router.post('/personalBooking', (req,res)=>{
     const personalBooking = new PersonalBooking({
-        user : req.body.user
+        user : req.body.user,
+        date : new Date(req.body.date),
+        startTime : new Date(req.body.startTime),
+        endTime : new Date(req.body.endTime),
+        title : req.body.title,
+        private : req.body.private,
+        description : req.body.description,
+        reminder : req.body.reminder,
+        reminderTime : new Date(req.body.reminderTime)
     });
     console.log(req.body.user);
-    personalBooking.save((err,data)=>{
-        if(err) res.send(err);
-        res.json(data)
-    });
-    /* .then(data => {
+    personalBooking.save()
+     .then(data => {
         res.json(data)
     })
     .catch(err => {
         res.json({message : err})
-    }) */
+    }) 
 });
 
 router.post('/groupBooking', (req,res)=>{
